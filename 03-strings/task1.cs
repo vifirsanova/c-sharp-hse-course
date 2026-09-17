883eea8026d71252a63d2c4174799781080f5405
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-17 12:23:02 +0300

    Create task1.cs

---
+++ 03-strings/task1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        // Form the greeting here
+    }
+}
