a74ea05c885c1ba401c45a67879ca67025746063
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-17 12:14:39 +0300

    Create task1.cs

---
+++ 02-operations/task1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Initial point coordinates
+        // Translation vector coordinates
+        // Calculate the new point coordinates
+        Console.WriteLine($"New point: ({newX}, {newY})");
+    }
+}
