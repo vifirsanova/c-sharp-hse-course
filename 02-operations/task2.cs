d7b87288b930d206c21d06cc4cdf2a09af770630
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-17 12:14:54 +0300

    Create task2.cs

---
+++ 02-operations/task2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Distance in meters
+        // Time in seconds
+        // Calculate the average speed
+        Console.WriteLine($"Average speed: {speed} m/s");
+    }
+}
