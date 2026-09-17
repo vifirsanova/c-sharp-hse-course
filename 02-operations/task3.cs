74d8d404aea35b7cf6875e6c4e0381f36d938542
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-17 12:15:08 +0300

    Create task3.cs

---
+++ 02-operations/task3.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Temperature in Celsius
+        // Convert Celsius to Fahrenheit
+        Console.WriteLine($"{celsius} C = {fahrenheit} F");
+    }
+}
