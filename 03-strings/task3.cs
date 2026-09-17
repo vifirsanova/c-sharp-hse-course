b4d1a3de1776cf8e2f56f51a32ff171f3bb0525b
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-17 12:22:25 +0300

    Create task3.cs

---
+++ 03-strings/task3.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string coords = "x=10;y=20;z=30";
+
+        // Divide the string and output each part
+    }
+}
