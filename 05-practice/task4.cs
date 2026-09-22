c54b0e935b25fde875f916e5a4ffc880c4302e07
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-22 19:58:48 +0300

    Create task4.cs

---
+++ 05-practice/task4.cs
@@ -0,0 +1,13 @@
+/*
+Задание 4: информация о 3D-модели
+Переменные: имя (`string`), полигоны (`int`), длительность анимации (`float`), видимость (`bool`), угол поворота (`float`).
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create variables here
+    }
+}
