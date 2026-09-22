5e69e0fcad146b0e4db9561699b9a9c1558c788b
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-22 19:51:44 +0300

    Create task3.cs

---
+++ 05-practice/task3.cs
@@ -0,0 +1,15 @@
+/*
+Задание 3: перевод температуры
+Формула: `°F = °C × 9/5 + 32`
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double celsius = 25.0;
+
+        // Convert to Fahrenheit
+    }
+}
