6f81df3961dc59d33341ef72815c28b9990c77ec
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-22 20:09:17 +0300

    Create task5.cs

---
+++ 05-practice/task5.cs
@@ -0,0 +1,16 @@
+/*
+Задание 5: преобразование типов
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double value = 7.85;
+
+        // Explicit conversion to int
+        // Implicit conversion back to double
+        // Compare values
+    }
+}
