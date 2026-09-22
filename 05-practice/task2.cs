fb7e221782204b2c810c3c3c19f43685b841f115
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-22 18:56:10 +0300

    Create task2.cs

---
+++ 05-practice/task2.cs
@@ -0,0 +1,18 @@
+/*
+Задание 2: средняя скорость и время
+Формула: `v = s / t`
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int distance = 1500;
+        int time = 120;
+
+        // Calculate speed (use double)
+        // Calculate time for 2500 meters
+    }
+}
+
