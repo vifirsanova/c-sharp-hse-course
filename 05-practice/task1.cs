b1514c3d1eb5de0f190626fab12e9e99987b8d09
vifirsanova <70071046+vifirsanova@users.noreply.github.com>
2026-09-22 18:55:39 +0300

    Create task1.cs

---
+++ 05-practice/task1.cs
@@ -0,0 +1,16 @@
+/*
+Задание 1: новые координаты
+Вывод: `New position: (x, y, z)`
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f, y = 3.0f, z = 1.5f;
+        float dx = 1.5f, dy = -2.0f, dz = 0.5f;
+
+        // Calculate new coordinates
+    }
+}
